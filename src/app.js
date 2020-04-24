18683fa3b9d98014ed705b52885fbf1f228e51f6
Juan Diego Acosta <juandiegoat01@gmail.com>
2020-04-24 15:50:35 -0500

    Hello World

--- src/app.js
+++ src/app.js
@@ -15,9 +15,16 @@ app.use(express.urlencoded({ extended : false }));
 
 
 // Routes
-
+app.use(require('./routes/index'));
 
 
 // Static Files
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// 404 handler
+app.use((req, res, next) => {
+    res.status(404).send('404 NotFound')
+})
 
 module.exports = app;
